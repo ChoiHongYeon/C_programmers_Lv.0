dead2226ce102434dc848843b9daf5e203d073c0
ChoiHongYeon <164014605+ChoiHongYeon@users.noreply.github.com>
2024-05-10 21:02:23 +0900

    first commit

---
+++ 20240510-1.c
@@ -0,0 +1,28 @@
+﻿// qr code(https://school.programmers.co.kr/learn/courses/30/lessons/181903?language=c)
+
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
+
+char* solution(int q, int r, const char* code) {
+    
+    int code_len = strlen(code);
+    int answer_len = 0;
+    
+    for (int i = 0; i < code_len; i++) {
+        if (i % q == r) {
+            answer_len++;
+        }
+    }
+    
+    char* answer = (char*)malloc(sizeof(char) * (answer_len + 1));
+
+    for (int i = 0; i < answer_len; i++) {
+        answer[i] = code[i * q + r];
+    }
+
+    answer[answer_len] = '\0';
+
+    return answer;
+}
