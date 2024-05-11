e4aca79767c48dc1713089b74cdaa5ed3df97075
ChoiHongYeon <164014605+ChoiHongYeon@users.noreply.github.com>
2024-05-11 20:32:53 +0900

    second commit

--- 20240511-1.c
+++ 20240511-1.c
@@ -10,20 +10,9 @@ int solution(int num_list[], size_t num_list_len) {
     for (int i = 0;i < num_list_len; i++) {
         if (num_list[i] < 0) {
             answer = i;
+            break;
         }
     }
 
     return answer;
-}
-
-void main()
-{
-    int A[] = { 12, 4, 15, 46, 38, -2, 15 };
-    int B[] = { 13, 22, 53, 24, 15, 6 };
-
-    size_t a = sizeof(A) / sizeof(A[0]);
-    size_t b = sizeof(B) / sizeof(B[0]);
-
-    printf("%d\n", solution(A, a));
-    printf("%d", solution(B, b));
 }
